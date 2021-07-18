215cf6a3aeae903d78995dccfd487fe78c387c43
AM-SAPP <8923499556nikhil@gmail.com>
2021-07-19 02:49:48 +0530

    about us with data of members

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import MemberCard from './MemberCard'
+import members from '../member';
 import './css/about.css'
 export default function Aboutus() {
     return (
         <div className="about-container">
             <h1>About Us</h1>
             <div className="mem-card-container">
-                <MemberCard/>
-                <MemberCard/>
-                <MemberCard/>
-                <MemberCard/>
+                {members.map((member,index)=>{
+                    return (
+                        <MemberCard key={index} name={member.name} image={member.image} linkedIn={member.linkedIn} instagram={member.instagram} github={member.github} />
+                    )
+                })}
             </div>
         </div>
     )
